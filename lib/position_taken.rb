5615502f968aab74c4e644afc017817afa3b7bf0
Sophia Fang <sophiaqfang@gmail.com>
2018-06-30 07:15:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " "
-    return false
-  else 
-    return true
-  end
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
